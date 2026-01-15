49ce13f7bf6282912a0984830464c18a175e5560
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-01-15 10:03:32 +0530

    Add solution for HackerRank problem in hackkerank7.java
    Implement solution for HackerRank problem involving loops and input processing.

---
+++ java-basics/loops/hackkerank7.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        
+        for (int i = 0; i < t; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int sum = a;
+            for (int j = 0; j < n; j++) {
+                sum += Math.pow(2, j) * b;
+                System.out.print(sum);
+                if (j < n - 1) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
