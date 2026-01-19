a4442c5a7b134687c26c3fbea0975f0e6c13228b
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-01-19 10:34:27 +0530

    Add area calculation for rectangle in Java

---
+++ Flexi-lecture/area-of-rectange.java
@@ -0,0 +1,15 @@
+
+public class One{
+
+public static void main(String args[]){
+int length = 4;
+int breadth = 6;
+int area;
+
+area  = length * breadth;
+
+System.out.println("Length " + length);
+System.out.println("breadth"  + breadth );
+System.out.println("area of rectangle" + area);
+}
+}
