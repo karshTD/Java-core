83ff7fc4b80791e8d3d4d5293bc747bc0d6cf1cf
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-01-26 09:38:52 +0530

    Implement string and number conversion methods
    Added methods for converting between strings and numbers, and handling character arrays.

---
+++ java-basics/Strings/convertString.java
@@ -0,0 +1,16 @@
+int num = 123;
+String numsStr = String.valueOf(num);
+String numStr = Integer.toString(num);
+
+//string to number
+String strNum = '456';
+int n = Integer.parseInt(strNum);
+double d = Double.parseDouble("12.34");   
+
+// Character array
+char[] chars = {'H', 'e', 'l', 'l', 'o'};
+String fromChars = new String(chars);     // "Hello"
+
+// String to char array
+String str = "Hello";
+char[] charArray = str.toCharArray();     // ['H','e','l','l','o']
