66053d35504ced8505d2f48118f8c25a9854073a
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-01-27 13:35:49 +0530

    Create MultilevelInheritence.java

---
+++ Flexi-lecture/MultilevelInheritence.java
@@ -0,0 +1,24 @@
+class Grandparent{
+    void showGrandparent(){
+        System.out.println("Grandparent class method");
+    }
+}
+
+class Parent extends Grandparent{
+    void showParent(){
+        System.out.println("Parent class method");
+    }
+}
+
+class Child extends Parent{
+    void showChild(){
+        System.out.println("Child class method");
+    }
+}
+
+public class MultilevelInheritence{
+    public static void main(String[] args){
+        Child c  = new Child();
+        c.showGrandparent();
+    }
+}
