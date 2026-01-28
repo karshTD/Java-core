9d1bf5e70f8b8f9d4daccdaa01bf3b72263739f8
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-01-28 20:11:06 +0530

    Delete Flexi-lecture/narrowCasting.java

--- Flexi-lecture/narrowCasting.java
+++
@@ -1,9 +0,0 @@
-public class Ncast{
-    public static void main(String args[]){
-        double myDouble = 9.78d;
-        int myInt = (int)myDouble;
-
-        System.out.println(myDouble);
-        System.out.println(myInt);
-    }
-}
