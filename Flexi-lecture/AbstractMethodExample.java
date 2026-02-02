373bcd9aed7e104c2ea48082378ebdaaf4369bee
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-02-02 11:27:20 +0530

    Create AbstractMethodExample.java

---
+++ Flexi-lecture/AbstractMethodExample.java
@@ -0,0 +1,19 @@
+abstract class Animal{
+    abstract void sound();
+    void eat(){
+        System.out.println("this animals eats");
+    }
+}
+
+class Dog extends Animal{
+    void sound(){
+        system.out.println("dog barks");
+    }
+}
+
+public class AbstractMethodExample{
+    public static void main(String[] args)
+    Animal animal = new Dog();
+    animal.sound();
+    animal.eat();
+}
