3b57315705afd5dc7ab5e6e05fef1d1baaf095ca
Utkarsh <132635254+karshTD@users.noreply.github.com>
2026-03-07 20:40:51 +0530

    Implement chained exception handling in ExCP.java

---
+++ Exception-Handeling/Chained-Exceptions/ExCP.java
@@ -0,0 +1,19 @@
+public class ExCP{
+    public static void main(String[] args){
+        try{
+            int[] n = new int[5];
+            int divisor = 0;
+
+            for(int i = 0; i < n.length; i++){
+                int res = n[i] / divisor;
+                System.out.println(res);
+
+
+            }
+        }
+        catch(ArithmeticException e){
+            throw new RuntimeException("error: division by zero occured" , e);
+
+        }
+    }
+}
